826635329e4ef202d12d76cb96961ea15683758b
urlee <urlee@ucsc.edu>
2016-11-02 16:15:18 -0700

    pseudo'd A*

--- src/craft_planner.py
+++ src/craft_planner.py
@@ -23,12 +23,14 @@ class State(OrderedDict):
 
     def __key(self):
         return tuple(self.items())
+    # (('apple', 3), ('berry', 2))
 
     def __hash__(self):
         return hash(self.__key())
 
     def __lt__(self, other):
         return self.__key() < other.__key()
+    # len(self) < len(other)
 
     def copy(self):
         new_state = State()
@@ -37,6 +39,10 @@ class State(OrderedDict):
 
     def __str__(self):
         return str(dict(item for item in self.items() if item[1] > 0))
+
+    def vk(self):
+        res = dict((v, k) for k, v in self.items())
+        return res
 
 
 def make_checker(rule):
@@ -153,14 +159,58 @@ def heuristic(state):
 
 
 def search(graph, state, is_goal, limit, heuristic):
-
     start_time = time()
+
+    # frontier = PriorityQueue()
+    queue = []  # HEAPQUEUE
+    start = state.__key()
+
+    # frontier.put(start, 0)
+    heappush(queue, (start, 0))  # MIGHT NEED TO SWITCH tuples!!!!
+    came_from = {}
+    cost_so_far = {}
+    came_from[initial] = None
+    cost_so_far[start] = 0
 
     # Implement your search here! Use your heuristic here!
     # When you find a path to the goal return a list of tuples [(state, action)]
     # representing the path. Each element (tuple) of the list represents a state
     # in the path and the action that took you to this state
-    while time() - start_time < limit:
+
+    # while not frontier.empty():
+    while time() - start_time < limit and len(queue) is not 0:
+
+        #     current = frontier.get()
+        #     if current == goal:
+        #           break
+        #     for next in graph.neighbors(current):
+        #           new_cost = cost_so_far[current] + graph.cost(current, next)
+        #           if next not in cost_so_far or new_cost < cost_so_far[next]:
+        #                cost_so_far[next] = new_cost
+        #                priority = new_cost + heuristic(goal, next)
+        #                frontier.put(next, priority)
+        #                came_from[next] = current
+
+        smallest = math.inf
+        rule_name, res_state, rule_cost = heappop(queue)
+
+        boxes[current_box] = []
+        for next_box in mesh['adj'][current_box]:
+            adjacent = adj_edge(current_box, next_box)
+            temp_point = shortest_to_adj(
+                current_point, adjacent[0], adjacent[1])
+            new_cost = dist[current_box] + distance(current_point, temp_point)
+            if next_box not in dist or new_cost < dist[next_box]:
+                dist[next_box] = new_cost
+                prev[next_box] = current_box
+                box_w_entry[next_box] = temp_point
+                t_to_d = heuristic(temp_point, destination_point)
+            priority = new_cost + t_to_d
+            heappush(queue, (priority, next_box))
+            boxes[current_box].extend(next_box)
+        contact_box = crosspath_search(prev, prev_b)
+        if contact_box is not None:
+            break
         pass
 
     # Failed to find a path
