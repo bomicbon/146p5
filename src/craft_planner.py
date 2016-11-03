65ab96a40e21f912a21393e35c1c2125b321ab45
urlee <urlee@ucsc.edu>
2016-11-02 20:23:16 -0700

    :(

--- src/craft_planner.py
+++ src/craft_planner.py
@@ -7,7 +7,7 @@
 import json
 from collections import namedtuple, defaultdict, OrderedDict
 from timeit import default_timer as time
-
+from heapq import heappush, heappop
 Recipe = namedtuple('Recipe', ['name', 'check', 'effect', 'cost'])
 
 
@@ -159,18 +159,26 @@ def heuristic(state):
 
 
 def search(graph, state, is_goal, limit, heuristic):
+    print('TESTING')
     start_time = time()
 
     # frontier = PriorityQueue()
     queue = []  # HEAPQUEUE
-    start = state.__key()
 
-    # frontier.put(start, 0)
-    heappush(queue, (start, 0))  # MIGHT NEED TO SWITCH tuples!!!!
+    # ----------------- 1 -------------------------------------
+    # MAY BE A PROBLEM HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    initial_name = ''
+    initial_state = state
+    initial_cost = 0
+    initial_baby = (initial_name, initial_state, initial_cost)
+
+    # ------------------------ 2 ---------------------------------
+    heappush(queue, initial_baby)  # MIGHT NEED TO SWITCH tuples!!!!
+
     came_from = {}
     cost_so_far = {}
-    came_from[initial] = None
-    cost_so_far[start] = 0
+    came_from[initial_baby] = None
+    cost_so_far[initial_baby] = 0
 
     # Implement your search here! Use your heuristic here!
     # When you find a path to the goal return a list of tuples [(state, action)]
@@ -179,38 +187,31 @@ def search(graph, state, is_goal, limit, heuristic):
 
     # while not frontier.empty():
     while time() - start_time < limit and len(queue) is not 0:
+        rule_name, res_state, rule_cost = heappop(queue)
+        popped_baby = (rule_name, res_state, rule_cost)
+        if is_goal:
+            backtracker =
 
-        #     current = frontier.get()
-        #     if current == goal:
-        #           break
-        #     for next in graph.neighbors(current):
+        graph_list = graph()  # (rule's name, resulting state, cost of rule)
+        for g in graph_list:
+            g_name = g[0]
+            g_state = g[1]
+            g_cost = g[2]
+            # --------------------- 3 -------------------------------
+            new_cost = cost_so_far[rule_name] + g_cost  # from current to next
+            if g_name not in cost_so_far or new_cost < cost_so_far[g_name]:
+                cost_so_far[g_name] = new_cost
+                priority = new_cost + heuristic(state)  # (goal, next)
+                new_baby = (g_name, g_state, priority)
+                came_from[g_name] = current
+                heappush(queue, new_baby)
+                came_from[g_name] = popped_baby
         #           new_cost = cost_so_far[current] + graph.cost(current, next)
         #           if next not in cost_so_far or new_cost < cost_so_far[next]:
         #                cost_so_far[next] = new_cost
         #                priority = new_cost + heuristic(goal, next)
         #                frontier.put(next, priority)
         #                came_from[next] = current
-
-        smallest = math.inf
-        rule_name, res_state, rule_cost = heappop(queue)
-
-        boxes[current_box] = []
-        for next_box in mesh['adj'][current_box]:
-            adjacent = adj_edge(current_box, next_box)
-            temp_point = shortest_to_adj(
-                current_point, adjacent[0], adjacent[1])
-            new_cost = dist[current_box] + distance(current_point, temp_point)
-            if next_box not in dist or new_cost < dist[next_box]:
-                dist[next_box] = new_cost
-                prev[next_box] = current_box
-                box_w_entry[next_box] = temp_point
-                t_to_d = heuristic(temp_point, destination_point)
-            priority = new_cost + t_to_d
-            heappush(queue, (priority, next_box))
-            boxes[current_box].extend(next_box)
-        contact_box = crosspath_search(prev, prev_b)
-        if contact_box is not None:
-            break
         pass
 
     # Failed to find a path
@@ -239,6 +240,7 @@ if __name__ == '__main__':
     all_recipes = []
 
     for name, rule in Crafting['Recipes'].items():
+        print(name)
         print(rule)
         checker = make_checker(rule)
         effector = make_effector(rule)
